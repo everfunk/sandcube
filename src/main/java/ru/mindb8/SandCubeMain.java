6cb278a06ee38014e8862640cbcae61947378ec6
Eugene Kuznetsov <EKuznetsov3@alfabank.ru>
2020-09-22 11:26:37 +0300

    remove unused declarations

--- src/main/java/ru/mindb8/SandCubeMain.java
+++ src/main/java/ru/mindb8/SandCubeMain.java
@@ -1,16 +1,10 @@
 package ru.mindb8;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import ru.mindb8.config.ApplicationConfig;
 
-@Slf4j
 public class SandCubeMain {
-
-
-
-    @SneakyThrows
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.register(ApplicationConfig.class);
--- src/main/java/ru/mindb8/config/ApplicationConfig.java
+++ src/main/java/ru/mindb8/config/ApplicationConfig.java
@@ -4,26 +4,10 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
-import org.springframework.beans.TypeConverter;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.mindb8.controller.MainController;
-
-import javax.servlet.ServletContext;
-import java.util.List;
 
 @Import({TomcatConfig.class, WebConfig.class})
 @RequiredArgsConstructor
